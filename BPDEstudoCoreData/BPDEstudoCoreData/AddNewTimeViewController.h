bb1d27d4e9b14178fecda5fae4be5e7d784c4523
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-05-19 08:11:23 -0400

    acabooooooooou

--- BPDEstudoCoreData/BPDEstudoCoreData/AddNewTimeViewController.h
+++ BPDEstudoCoreData/BPDEstudoCoreData/AddNewTimeViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "BPDTimes.h"
+#import <CoreData/CoreData.h>
 
 @interface AddNewTimeViewController : UIViewController <UITextFieldDelegate>
 
--- BPDEstudoCoreData/BPDEstudoCoreData/AppDelegate.m
+++ BPDEstudoCoreData/BPDEstudoCoreData/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "BPDTimesStore.h"
 
 @interface AppDelegate ()
 
@@ -16,6 +17,8 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [[BPDTimesStore sharedStore] setManagedObjectContext:
+     [self managedObjectContext]];
     // Override point for customization after application launch.
     return YES;
 }
